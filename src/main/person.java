c67123db6fb0af411ac770f0a438e55c00d1808c
gitDeveloper2 <kamaujosephhorace@gmail.com>
2023-02-05 18:11:17 +0300

    select

--- src/main/person.java
+++ src/main/person.java
@@ -1,7 +1,35 @@
 package main;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+
 @Entity
 
-public class Origin {
+public class person {
+    @Id
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName (String name) {
+        this.name = name;
+    }
+
+
+    public String String() {
+        return "person{" +
+                "id=" + id +
+                ", name=" + name +
+                '}';
+    }
 }
